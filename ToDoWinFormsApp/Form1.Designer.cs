a2acda5043a5f98a15311e243d9f7c471d9f6f3b
AlexKonev <alexkonev777@gmail.com>
2023-03-22 19:30:26 +0300

    designed

--- ToDoWinFormsApp/Form1.Designer.cs
+++ ToDoWinFormsApp/Form1.Designer.cs
@@ -23,36 +23,70 @@
         ///  the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.lstTasks = new System.Windows.Forms.CheckedListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.txtTask = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // checkedListBox1
+            // lstTasks
             // 
-            this.checkedListBox1.FormattingEnabled = true;
-            this.checkedListBox1.HorizontalScrollbar = true;
-            this.checkedListBox1.Items.AddRange(new object[] {
-            "2 2 2",
-            "bbb",
-            ";;;;"});
-            this.checkedListBox1.Location = new System.Drawing.Point(451, 237);
-            this.checkedListBox1.Name = "checkedListBox1";
-            this.checkedListBox1.Size = new System.Drawing.Size(120, 79);
-            this.checkedListBox1.TabIndex = 0;
+            this.lstTasks.FormattingEnabled = true;
+            this.lstTasks.HorizontalScrollbar = true;
+            this.lstTasks.Location = new System.Drawing.Point(287, 12);
+            this.lstTasks.Name = "lstTasks";
+            this.lstTasks.Size = new System.Drawing.Size(120, 329);
+            this.lstTasks.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(36, 270);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(155, 46);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Add Task";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonSave.Location = new System.Drawing.Point(12, 12);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(95, 56);
+            this.buttonSave.TabIndex = 2;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // txtTask
+            // 
+            this.txtTask.Location = new System.Drawing.Point(36, 149);
+            this.txtTask.Name = "txtTask";
+            this.txtTask.Size = new System.Drawing.Size(155, 30);
+            this.txtTask.TabIndex = 3;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 23F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.checkedListBox1);
+            this.ClientSize = new System.Drawing.Size(563, 344);
+            this.Controls.Add(this.txtTask);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lstTasks);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private CheckedListBox checkedListBox1;
+        private CheckedListBox lstTasks;
+        private Button button1;
+        private Button buttonSave;
+        private TextBox txtTask;
     }
 }
--- ToDoWinFormsApp/Form1.cs
+++ ToDoWinFormsApp/Form1.cs
@@ -3,5 +3,17 @@ namespace ToDoWinFormsApp {
         public Form1() {
             InitializeComponent();
         }
+
+        private void buttonSave_Click(object sender, EventArgs e) {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            string newTask = txtTask.Text;
+            if (!string.IsNullOrWhiteSpace(newTask)) {
+                lstTasks.Items.Add(newTask);
+                txtTask.Text = "";
+            }
+        }
     }
 }
